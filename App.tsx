2a701cd9b0a2e9866eef7d89f025a93f8ca7d402
Alice Girard Guittard <alice.girard@protonmail.com>
2019-09-03 22:31:36 +0200

    native-base header

--- App.tsx
+++ App.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { Container, Title, Header, Left, Body, Right } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+interface State {
+  isReady: boolean;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+interface Props {
+}
+
+export default class App extends React.Component<Props, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
+    return (
+      <Container>
+        <Header>
+          <Left/>
+          <Body>
+            <Title>OpenMood?</Title>
+          </Body>
+          <Right/>
+        </Header>
+      </Container>
+    );
+  }
+}
